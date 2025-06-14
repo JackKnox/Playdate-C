e1854c317bfd385ec26a3004846603b6b75e59e8
Jack Knox <jackjackfilms1@gmail.com>
2025-06-14 07:59:42 +0100

    Update

--- Source/main.c
+++ Source/main.c
@@ -1,15 +1,16 @@
-//
-//  main.c
-//  Extension
-//
-//  Created by Dave Hayden on 7/30/14.
-//  Copyright (c) 2014 Panic, Inc. All rights reserved.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 
+// The Playdate api
 #include "pd_api.h"
+
+#define TEXT_WIDTH 86
+#define TEXT_HEIGHT 16
+
+int x = (400-TEXT_WIDTH)/2;
+int y = (240-TEXT_HEIGHT)/2;
+int dx = 1;
+int dy = 2;
 
 static int update(void* userdata);
 const char* fontpath = "/System/Fonts/Asheville-Sans-14-Bold.pft";
@@ -22,7 +23,7 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 {
 	(void)arg; // arg is currently only used for event = kEventKeyPressed
 
-	if ( event == kEventInit )
+	if (kEventInit == event)
 	{
 		const char* err;
 		font = pd->graphics->loadFont(fontpath, &err);
@@ -30,25 +31,17 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
 		if ( font == NULL )
 			pd->system->error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, fontpath, err);
 
-		// Note: If you set an update callback in the kEventInit handler, the system assumes the game is pure C and doesn't run any Lua code in the game
+		// Note: If you set an update callback in the kEventInit handler, 
+		//       the system assumes the game is pure C and doesn't run any Lua code in the game.
 		pd->system->setUpdateCallback(update, pd);
 	}
 	
 	return 0;
 }
 
-
-#define TEXT_WIDTH 86
-#define TEXT_HEIGHT 16
-
-int x = (400-TEXT_WIDTH)/2;
-int y = (240-TEXT_HEIGHT)/2;
-int dx = 1;
-int dy = 2;
-
 static int update(void* userdata)
 {
-	PlaydateAPI* pd = userdata;
+	PlaydateAPI* pd = (PlaydateAPI*)userdata;
 	
 	pd->graphics->clear(kColorWhite);
 	pd->graphics->setFont(font);
@@ -67,4 +60,3 @@ static int update(void* userdata)
 
 	return 1;
 }
-
